14d43c3cdd79f569317a3952714d83f5e308f8fc
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-13 05:08:27 +0000

    made a simple https request to example.org, using console.log to explore when the different lines trigger

--- https_example.js
+++ https_example.js
@@ -1,3 +1,17 @@
 var https = require('https');
 
-console.log('i did it!');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made
+var callback = function() {
+  console.log('In response handler callback');
+};
+
+console.log('About to make the request');
+
+https.request(options, callback).end();
+
+console.log('Made the request!');
